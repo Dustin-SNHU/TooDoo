1fd6ec2856c8119ee8f2557cb6036afb59e27534
Dustin Richardson <dustinrichardson92@gmail.com>
2022-01-11 07:41:42 -0500

    Initial Commit

---
+++ TooDoo/TooDooApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooDooApp.swift
+//  TooDoo
+//
+//  Created by Dustin Richardson on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TooDooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
