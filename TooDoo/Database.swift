6d87709721a31d392df5fc593b104408ebeff4a4
Dustin-SNHU <dustin.richardson@snhu.edu>
2022-01-19 16:42:13 -0500

    Created variables and database objects. Database template complete.

--- TooDoo/Database.swift
+++ TooDoo/Database.swift
@@ -6,7 +6,20 @@
 //
 //  Description: This file will host items that are added to the TooDoo task list and act as a database.
 
-//Import Functions
+// Import Functions
 import Foundation // Default file import statement
 import SwiftUI // Allows use of Swift UI
 import Combine // Allows application page to communicate with main page
+
+// Create structure to hold task items
+struct Task: Identifiable {
+    var id = String() // Unique ID
+    var taskName = String() // Name of task item
+    var taskDescription = String() // Description of task item
+    //hold for date field
+}
+
+// Create Database ObservableObject to store task items 
+class Database : ObservableObject {
+    @Published var tasks = [Task]()
+}
