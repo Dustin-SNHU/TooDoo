8363a01a8a205cccbc2153a8d9a1bc19f08ddaf3
Dustin-SNHU <dustin.richardson@snhu.edu>
2022-01-19 16:54:15 -0500

    Added newTaskItem textField for receiving user input.

--- TooDoo/ContentView.swift
+++ TooDoo/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 // Main structure
 struct ContentView: View {
     @ObservedObject var taskDatabase = TaskDatabase() // Retrieve list items
+    @State var newTaskItem : String = "" // Updatable object for new task list item
+    
+    // textField structure
+    var textField : some View {
+        HStack {
+            TextField("Enter Task Name",text:
+                        self.$newTaskItem)
+        }
+    }
+    
+    // Task list array
     var body: some View {
         NavigationView {
             VStack {
                 List(self.taskDatabase.tasks) {
                     task in Text(task.taskName) // Pull task names
                 }
+                .navigationBarTitle("TooDoo") // Title element
             }
         }
     }
