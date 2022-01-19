03a7052490d3a7adec06edac324a9f5efc26f450
Dustin-SNHU <dustin.richardson@snhu.edu>
2022-01-19 16:38:19 -0500

    Added import statements.

--- TooDoo/Database.swift
+++ TooDoo/Database.swift
@@ -4,5 +4,9 @@
 //
 //  Created by Dustin Richardson on 1/19/22.
 //
+//  Description: This file will host items that are added to the TooDoo task list and act as a database.
 
-import Foundation
+//Import Functions
+import Foundation // Default file import statement
+import SwiftUI // Allows use of Swift UI
+import Combine // Allows application page to communicate with main page
