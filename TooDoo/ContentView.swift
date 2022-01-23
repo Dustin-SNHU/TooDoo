55fed839a1898a60f1996d330f86b3e9a600d4cb
Dustin-SNHU <dustin.richardson@snhu.edu>
2022-01-23 11:15:28 -0500

    Added Edit button to application.

--- TooDoo/ContentView.swift
+++ TooDoo/ContentView.swift
@@ -5,20 +5,24 @@
 //  Created by Dustin Richardson on 1/11/22.
 //
 
+// Import statements
 import SwiftUI
+import Combine
 
 // Main structure
 struct ContentView: View {
     @ObservedObject var taskDatabase = TaskDatabase() // Retrieve list items
-    @State var newTaskItem : String = "" // Updatable object for new task list item
+    @State var newTaskItem : String = "" // Updatable object for new task list item; will update as the user types into the textField
     
     // textField structure
     var textField : some View {
         HStack {
             TextField("Enter Task Name",text: self.$newTaskItem)
+            
             // Button to add new task items
             Button(action: self.addNewTask, label: {
                 Text("New")
+                
             })
         }
     }
@@ -35,13 +39,20 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                textField.padding()
-                List(self.taskDatabase.tasks) {
-                    task in Text(task.taskName) // Pull task names
+                textField.padding() // Makes button look nicer
+                List {
+                    ForEach(self.taskDatabase.tasks) {
+                        task in Text(task.taskName)
+                    }.onMove(perform: self.move)
                 }
                 .navigationBarTitle("TooDoo") // Title element
+                .navigationBarItems(trailing: EditButton()) // Edit button
             }
         }
+    }
+    func move(from source: IndexSet, to destination: Int) {
+        taskDatabase.tasks.move(fromOffsets: source, toOffset: destination)
+        
     }
 }
 
